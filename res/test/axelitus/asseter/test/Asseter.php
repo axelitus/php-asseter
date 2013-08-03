0d54d5c1972ff37b8729737aafed79632ed4a56f
Axel Pardemann <axelitusdev@gmail.com>
2013-08-03 14:17:35 -0500

    Configured PHPUnit tests.

---
+++ res/test/axelitus/asseter/test/Asseter.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Part of the axelitus\Asseter Package.
+ *
+ * @package     axelitus\Asseter
+ * @version     0.1
+ * @author      Axel Pardemann (axelitusdev@gmail.com)
+ * @license     MIT License
+ * @copyright   2013 - Axel Pardemann
+ * @link        http://axelitus.mx/projects/asseter
+ */
+
+namespace axelitus\asseter\test;
+
+class Test_Asseter extends TestCase
+{
+
+}
---
+++ res/test/axelitus/asseter/test/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Part of the axelitus\Asseter Package.
+ *
+ * @package     axelitus\sseter
+ * @version     0.1
+ * @author      Axel Pardemann (axelitusdev@gmail.com)
+ * @license     MIT License
+ * @copyright   2013 - Axel Pardemann
+ * @link        http://axelitus.mx/projects/asseter
+ */
+namespace axelitus\asseter\test;
+
+class TestCase extends \PHPUnit_Framework_TestCase { }
