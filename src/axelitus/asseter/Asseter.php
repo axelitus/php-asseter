58b4f06cba5f049d11db0988917c20e0aced64cf
Axel Pardemann <axelitusdev@gmail.com>
2013-08-03 14:21:05 -0500

    Set the class shell for the Asseter class.

---
+++ src/axelitus/asseter/Asseter.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Part of the axelitus\Asseter Package.
+ *
+ * @package     axelitus\Asseter
+ * @version     0.1
+ * @author      Axel Pardemann (axelitusdev@gmail.com)
+ * @license     MIT License
+ * @copyright   2013 - Axel Pardemann
+ * @link        http://axelitus.mx/projects/asseter
+ */
+
+namespace axelitus\asseter;
+
+/**
+ * Class Asseter
+ * @package axelitus\Asseter
+ */
+class Asseter
+{
+
+}
