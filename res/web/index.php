5b326a9d9e36de9689b91e34652afcd0245d33c9
Axel Pardemann <axelitusdev@gmail.com>
2013-08-03 14:19:26 -0500

    Set a manual test dir in the res directory to test this in a web environment.

---
+++ res/web/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Part of the axelitus\Asseter Package.
+ *
+ * @package     axelitus\Asseter
+ * @version     0.1
+ * @author      Axel Pardemann (axelitusdev@gmail.com)
+ * @license     MIT License
+ * @copyright   2013 - Axel Pardemann
+ * @link        http://axelitus.mx/projects/asseter
+ */
+
+/**
+ * This folder is destined to manual tests to see what the package is doing.
+ */
+
+echo "Package: axelitus\Asseter";
